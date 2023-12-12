50cb37aeab65e070645bbaf5f4d0b0cd721d6d62
ummetarin <ummehomairatarin@gmail.com>
2023-12-12 13:46:27 +0600

    added link in it

--- src/component/Home/Banner.jsx
+++ src/component/Home/Banner.jsx
@@ -20,6 +20,20 @@ const Banner = () => {
              
          </Typewriter></h1>
       <p className="py-5 text-sm font-normal text-white">My journey into the world of web development began with a curiosity about creating visually stunning and user-friendly interfaces. Currently pursuing my studies in CSS, I am on a mission to blend creativity with technical skills to build immersive digital experiences.</p>
+
+      <div className='text-black flex flex-row items-center font-bold gap-4 '>
+        <img className='h-4' src="https://i.ibb.co/kBxy3py/download-removebg-preview.png" alt="" />
+      <p><a href='https://www.facebook.com/farjanaakter.apple'>Umme Homaira</a></p>
+      </div>
+      <div className='text-black flex flex-row items-center font-bold gap-4 pt-2 '>
+        <img className='h-4' src="https://i.ibb.co/DVJJ9jn/png-clipart-mailing-envelope-logo-email-marketing-computer-icons-email-miscellaneous-web-design-remo.png" alt="" />
+      <p><a href=''>ummehomairatarin@gmail.com</a></p>
+      </div>
+      <div className='text-black flex flex-row items-center font-bold gap-4 pt-2 pb-6 '>
+        <img className='h-4' src="https://i.ibb.co/3zt5y9C/images-removebg-preview-1.png" alt="" />
+      <p><a href='https://bd.linkedin.com/in/umme-homaira-tarin-2617372a0'>Umme Homaira</a></p>
+      </div>
+
       <button className="btn bg-orange-600 text-white rounded-full">Dowload CV</button>
     </div>
   </div>
